f7db5167fde26a78875df98f406229fa5863b966
ArianIslam <arian.islam024@gmail.com>
2022-08-27 12:32:47 +0600

    Stacks and Queues Added

---
+++ stack.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class Stack:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.top=new_node
+        self.height=1
+        return True
+    
+    def push(self,value):
+        new_node=Node(value)
+        if self.height is 0:
+            self.top=new_node
+        else:
+            new_node.next=self.top
+            self.top=new_node
+        self.height+=1
+        return True
+        
+    def pop(self):
+        if self.height is 0:
+            return None
+        elif self.height is 1:
+            self.top=None
+        else:
+            temp=self.top
+            self.top=temp.next
+            temp.next=None
+        self.height-=0
+        return temp.value
+        
+
+    def print_stack(self):
+        temp = self.top
+        if temp is None:
+            print("No item in Stack.")
+        else:
+            while temp:
+                print(temp.value)
+                temp=temp.next
+    
+    
